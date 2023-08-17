5a89e0f79d6d1d81250efdf704030111fad5a220
Natnael <>
2023-08-17 11:06:48 +0200

    fix doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 
+# Create a Flask web app
 from flask import Flask
+
 app = Flask(__name__)
 
-# Create a Flask web app
+
 @app.route('/', strict_slashes=False)
 def hello():
-    """
-    Returns a greeting message.
-
-    This function returns a plain text greeting message, 'Hello HBNB!',
-    which can be used to greet users or provide a simple welcome.
-
-    Returns:
-        str: A greeting message.
-    """
     return 'Hello HBNB!'
 
 

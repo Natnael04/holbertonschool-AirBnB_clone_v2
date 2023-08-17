2fe919e733798cbf9e6a102f61da416e0c7efeb6
Natnael <>
2023-08-17 10:41:19 +0200

    fix the documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,9 +3,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Create a Flask web app
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

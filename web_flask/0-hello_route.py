6c743bdf68c0d0f4c496ab62bf52ef6a8d708774
Natnael <>
2023-08-17 10:58:04 +0200

    fix doc

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,16 +4,13 @@ from flask import Flask
 app = Flask(__name__)
 
 # Create a Flask web app
-
-
 @app.route('/', strict_slashes=False)
 def hello():
     """
-    A route function that returns a simple greeting message.
+    Returns a greeting message.
 
-    This function is associated with the root URL ('/'). When a user
-    accesses the root URL, this function will be invoked, and it will
-    return a plain text message saying "Hello, World!".
+    This function returns a plain text greeting message, 'Hello HBNB!',
+    which can be used to greet users or provide a simple welcome.
 
     Returns:
         str: A greeting message.
@@ -22,19 +19,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    """
-    Entry point for running the Flask app.
-
-    This block of code checks if the script is being run directly (as
-    opposed to being imported as a module). If it's the main script,
-    it starts the Flask development server, allowing the app to be
-    accessed via a web browser.
-
-    Note:
-        The development server is only meant for testing purposes and
-        should not be used in a production environment.
-
-    Usage:
-        Run this script directly using Python to start the Flask app.
-    """
     app.run(host='0.0.0.0', port=5000)
